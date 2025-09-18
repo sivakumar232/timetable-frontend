e154a45e5ac31b9d8b9043e24ec1fa455dd787e7
Sivakumar <sivakumarr3105@gmail.com>
2025-09-18 18:43:25 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import TimetableTester from './TimetableTester';
+import React from 'react';
+function App() {
+  return (
+    <main>
+      <TimetableTester />
+    </main>
+  );
+}
+
+export default App;
